7987fe1fbdc61c61385ecc3f49d5c9bdad48a679
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2024-03-03 22:59:59 -0500

    basic player movement and animations

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform followTarget;
+    [SerializeField] float catchUpSpeed = 1;
+    [Header("Pitch clamping")]
+    [Range(-90, 90)]
+    [SerializeField] float camMinClamp = -90;
+    [Range(-90, 90)]
+    [SerializeField] float camMaxClamp = 90;
+
+    [Header("Drift behind player")]
+    [Tooltip("Speed at which the camera drifts behind the player when moving and not touching the camera")]
+    [SerializeField] float driftSpeed = 10;
+
+    [Tooltip("Time before drift fully kicks in")]
+    [SerializeField] float easeInTime = 1;
+
+    [Range(0, 5)]
+    [Tooltip("Time before camera drift starts kicking in")]
+    [SerializeField] float driftTimer = 1;
+
+    [SerializeField] float driftMinSpeed = 10;
+
+    [SerializeField] float speedToReachMaxDrift = 10;
+
+    [Range(0, 2)]
+    [SerializeField] float followTargetVerticalOffset = 0;
+
+    [SerializeField] float controllerSensitivity = 140;
+    [SerializeField] float mouseSensitivity = 3;
+    Vector2 lookInput = Vector2.zero;
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    public float CamMinClamp => camMinClamp;
+    public float CameraMaxClamp => camMaxClamp;
+
+    float driftTime = 0;
+
+    PlayerInput playerInput;
+
+    public string CurrentControlScheme { get => playerInput.currentControlScheme; }
+    public bool KeyboardAndMouseActive { get => CurrentControlScheme == "Keyboard&Mouse"; }
+    public bool GamepadActive { get => CurrentControlScheme == "Gamepad"; }
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        followTarget.parent = null;
+        //GetComponentInChildren<CinemachineVirtualCamera>().transform.parent = null;
+    }
+    void Update()
+    {
+        followTarget.position = transform.position + Vector3.up * followTargetVerticalOffset;
+        float appliedSens = GamepadActive ? controllerSensitivity : mouseSensitivity;
+
+        // Quaternion * Quaternion is the same as applying rotation from second to first
+        Quaternion cameraRotation = followTarget.transform.localRotation *= Quaternion.AngleAxis(lookInput.x * appliedSens * Time.deltaTime, Vector3.up);
+
+        cameraRotation *= Quaternion.AngleAxis(-lookInput.y * appliedSens * Time.deltaTime, Vector3.right);
+
+        Vector3 cameraAngles = cameraRotation.eulerAngles;
+
+        cameraAngles.z = 0;
+
+        // convert [0,360[ degrees to ]-180:180] degrees to avoid looping at 0 and allowing negative angles.
+        // 0 degrees is direction of original angle when camera looks at player
+        cameraAngles.x = cameraAngles.x > 180 ? cameraAngles.x - 360 : cameraAngles.x;
+        cameraAngles.x = Mathf.Clamp(cameraAngles.x, camMinClamp, camMaxClamp);
+
+        followTarget.transform.localEulerAngles = new Vector3(cameraAngles.x, cameraAngles.y, 0);
+
+        ApplyCameraDrift();
+    }
+
+    private void ApplyCameraDrift()
+    {
+
+        // Moves Camera behind the player when no look input is given
+        //Vector3 movementDirection = rb.velocity;
+        //movementDirection.y = 0;
+        //if (lookInput.magnitude == 0)
+        //{
+        //    driftTime += Time.deltaTime;
+        //    if (driftTime < driftTimer) return;
+
+        //    Vector2 camDirection = new Vector2(followTarget.transform.forward.x, followTarget.transform.forward.z);
+        //    Vector2 playerDirection = new Vector2(rb.velocity.x, rb.velocity.z).normalized;
+
+        //    float dot = Vector2.Dot(playerDirection, camDirection);
+
+        //    float dotFactor = (1 - Mathf.Abs(dot)) * Mathf.Clamp01((driftTime - driftTimer) / easeInTime);
+        //    float speedFactor = Mathf.Clamp(movementDirection.magnitude, 0, speedToReachMaxDrift) / speedToReachMaxDrift;
+        //    Quaternion to = Quaternion.LookRotation(movementDirection.normalized + Vector3.down * .5f, Vector3.up);
+        //    followTarget.transform.rotation = Quaternion.Lerp(followTarget.transform.rotation, to, Time.deltaTime * dotFactor * driftSpeed * speedFactor);
+        //}
+        //else
+        //    driftTime = 0;
+    }
+    public void SyncFollowTarget()
+    {
+        followTarget.position = transform.position + Vector3.up * followTargetVerticalOffset;
+    }
+
+    public void ResetRotation()
+    {
+        followTarget.transform.rotation = transform.rotation * Quaternion.Euler(30, 0, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,211 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController characterController;
+    [SerializeField] float walkingSpeed;
+    [SerializeField] float runningSpeed;
+    [SerializeField] float sprintSpeed;
+    [Range(0, 1)]
+    [SerializeField] float runThreshold = .7f;
+    [Range(0, 1)]
+    [SerializeField] float deadZone = .1f;
+    [Range(0, 50)]
+    [SerializeField] float acceleration = 5;
+    [Range(0,50)]
+    [SerializeField] float deceleration = 15;
+    [Tooltip("Stamina/s")]
+    [SerializeField] float sprintStaminaCost = 15;
+    [SerializeField] float timeToConsiderFalling = .5f;
+    float currentSpeed = 0;
+    new Camera camera;
+    Vector2 movementInput;
+    Vector3 direction = Vector3.forward;
+    float dropSpeed = 0;
+    public bool movementFrozen = false;
+    public bool rotationFrozen = false;
+    public bool movementReduced = false;
+    float targetSpeed = 0;
+    bool wasSprinting = false;
+    bool isDecelerating = false;
+    [SerializeField] float turnSpeed = 100;
+    Vector2 prevInput = Vector2.zero;
+    bool isGrounded = false;
+    [SerializeField] float groundCheckDistance = .8f;
+    [SerializeField] LayerMask groundLayer;
+    bool wasFalling = false;
+    float timeSinceWasGrounded = 0;
+    Animator animator;
+
+    public bool IsSprinting { get; private set; } = false;
+    Vector2 previousMovement = Vector2.zero;
+    public float Gravity
+    {
+        get
+        {
+            dropSpeed += Physics.gravity.y * Time.deltaTime;
+            return dropSpeed * Time.deltaTime;
+        }
+    }
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        camera = Camera.main;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movementFrozen = animator.GetBool("Busy");
+        characterController.Move(new Vector3(0, Gravity, 0));
+        isGrounded = characterController.isGrounded;
+        bool isFalling = !isGrounded;
+
+        if (characterController.isGrounded)
+        {
+            dropSpeed = -1f;
+            timeSinceWasGrounded = 0;
+        }
+        else
+        {
+            timeSinceWasGrounded += Time.deltaTime;
+            if (timeSinceWasGrounded > timeToConsiderFalling)
+                isFalling = !(dropSpeed < 0 && Physics.Raycast(characterController.transform.position, Vector3.down, groundCheckDistance, groundLayer));
+            else isFalling = false;
+        }
+
+
+        wasFalling = isFalling;
+        animator.SetBool("IsFalling", isFalling);
+
+        Vector3 relativeMovement = Time.deltaTime * walkingSpeed * new Vector3(movementInput.x, 0, movementInput.y);
+
+        Vector4 absoluteMovement = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0) * relativeMovement; //handle camera rotation
+        Quaternion movementForward;
+
+        if (absoluteMovement.magnitude > 0)
+        {
+            direction = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0) * new Vector3(movementInput.x, 0, movementInput.y);
+        }
+        // IsSprinting = inputInterface.IsSprinting && inputInterface.Move.magnitude >= runThreshold && stamina.CanRun && !movementReduced && animationEvents.ActionAvailable;
+
+        if (!rotationFrozen && direction.magnitude > 0)
+        {
+            movementForward = Quaternion.LookRotation(direction, Vector3.up);
+            characterController.transform.rotation = Quaternion.RotateTowards(characterController.transform.rotation, movementForward, turnSpeed * Time.deltaTime);
+        }
+
+        if (!movementFrozen)
+        {
+            animator.SetBool("IsWalking", !movementFrozen && absoluteMovement.magnitude > 0);
+            characterController.Move(absoluteMovement);
+        }
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            animator.SetTrigger("Scratch");
+    }
+
+    //void HandleMovement()
+    //{
+    //    Vector2 movementInput = inputInterface.Move;
+    //    if ((prevInput - movementInput).magnitude > 1.05)
+    //    {
+    //        movementInput = prevInput;
+    //        prevInput = Vector2.zero;
+    //    }
+    //    else
+    //        prevInput = movementInput;
+    //    float movementMagnitude = movementInput.magnitude;
+    //    IsSprinting = inputInterface.IsSprinting && movementMagnitude >= runThreshold && stamina.CanRun && !movementReduced && animationEvents.ActionAvailable;
+    //    animator.SetBool("IsSprinting", IsSprinting);
+    //    if (movementFrozen)
+    //    {
+    //        direction = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0) * new Vector3(movementInput.x, 0, movementInput.y);
+    //        animator.SetBool("IsMoving", false);
+    //    }
+    //    else if (movementMagnitude >= deadZone) 
+    //    {
+    //        animator.SetBool("IsMoving", true);
+    //        direction = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0) * new Vector3(movementInput.x, 0, movementInput.y);
+
+
+    //        if (IsSprinting)
+    //        {
+    //            stamina.Remove(sprintStaminaCost * Time.deltaTime, true);
+    //            targetSpeed = targetSpeed > sprintSpeed ? sprintSpeed : targetSpeed + Time.deltaTime * acceleration;
+    //            targetSpeed = targetSpeed > sprintSpeed ? sprintSpeed : targetSpeed;
+    //        }
+    //        else if (movementMagnitude >= runThreshold && !movementReduced)
+    //        {
+    //            targetSpeed = runningSpeed;
+    //            animator.SetFloat("MovementSpeedMultiplier", runningSpeed / walkingSpeed);
+    //        }
+    //        else
+    //        {
+    //            targetSpeed = walkingSpeed;
+    //            animator.SetFloat("MovementSpeedMultiplier", 1);
+    //        }
+
+    //        if (lockOn.IsLocked && !inputInterface.IsSprinting)
+    //        {
+    //            Vector2 blending = movementInput.normalized;
+    //            animator.SetFloat("MovementX", blending.x);
+    //            animator.SetFloat("MovementY", blending.y);
+    //        }
+    //        else
+    //        {
+    //            animator.SetFloat("MovementX", 0);
+    //            animator.SetFloat("MovementY", 1);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        targetSpeed = 0;
+    //        animator.SetBool("IsMoving", false);
+    //    }
+
+    //    if (wasSprinting && !IsSprinting) StartCoroutine(Decelerate());
+    //    wasSprinting = IsSprinting;
+    //    if (!isDecelerating)
+    //    {
+    //        currentSpeed = targetSpeed;
+    //        movementInput = currentSpeed * Time.deltaTime * movementInput.normalized;
+    //    }
+    //    else if (movementMagnitude < deadZone)
+    //    {
+    //        movementInput = currentSpeed * Time.deltaTime * previousMovement;
+    //    }
+    //    else
+    //        movementInput = currentSpeed * Time.deltaTime * movementInput.normalized;
+    //    movement.x = movementInput.x;
+    //    movement.z = movementInput.y;
+    //    if (!movementFrozen && movementMagnitude >= deadZone)
+    //        previousMovement = movementInput.normalized;
+    //}
+
+    IEnumerator Decelerate()
+    {
+        isDecelerating = true;
+        while (targetSpeed < currentSpeed)
+        {
+            yield return null;
+            currentSpeed -= Time.deltaTime * deceleration; 
+        }
+        currentSpeed = targetSpeed;
+        isDecelerating = false;
+    }
+}
