b9a8a915cae37fb31aa8b44433d46ca4a784c29c
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2024-03-05 00:10:00 -0500

    improved feel and corrected busy animations on slopes

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -1,4 +1,11 @@
-﻿public static class Extensions
-{
+﻿using UnityEngine;
 
+public static class Extensions
+{
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        float start = (min + max) * 0.5f - 180;
+        float floor = Mathf.FloorToInt((angle - start) / 360) * 360;
+        return Mathf.Clamp(angle, min + floor, max + floor);
+    }
 }
--- Assets/Scripts/FoxAnimations.cs
+++ Assets/Scripts/FoxAnimations.cs
@@ -7,20 +7,28 @@ using UnityEngine;
 
 public class FoxAnimations : MonoBehaviour
 {
-    [SerializeField] Transform root, thighL, thighR, shoulderL, shoulderR, neck, pivotPoint, targetRotation;
+    [SerializeField] Transform root, trapezius, thighL, thighR, shoulderL, shoulderR, neck, pivotPoint, targetRotation;
     [SerializeField] LayerMask groundLayer = 1;
     Animator animator;
-    [SerializeField] float maxDistanceDelta = 60;
-    [SerializeField] float maxDegreesDelta = 2;
+    [SerializeField] float rootRotationSpeed = 300;
+    [SerializeField] float neckRotationSpeed = 200;
 
-    Vector3 positionProgress;
-    Quaternion rotationProgress;
+    Quaternion rootRotationProgress;
+    float neckRotationProgress;
+
+    float localNeckRestingAngle;
+    [SerializeField] float maxNeckDeltaAngle = 30;
+
+    Vector3 currentTranslationDiff = Vector3.zero;
+    Quaternion currentRotationDiff = Quaternion.identity;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
         targetRotation.parent = null;
-        positionProgress = root.position;
-        rotationProgress = root.rotation;
+        rootRotationProgress = root.rotation;
+        localNeckRestingAngle = neck.localEulerAngles.x;
+        neckRotationProgress = localNeckRestingAngle;
     }
 
     private void LateUpdate()
@@ -28,31 +36,44 @@ public class FoxAnimations : MonoBehaviour
         bool busy = animator.GetBool("Busy");
         if (busy)
         {
-            AdjustToInclineSmoothly();
+            rootRotationProgress = Quaternion.RotateTowards(rootRotationProgress, currentRotationDiff * root.rotation, rootRotationSpeed * Time.deltaTime);
+            root.SetPositionAndRotation(currentTranslationDiff + root.position, rootRotationProgress);
             return;
         }
-        Vector3 thighOrigin = (thighL.position + thighR.position) / 2;
-        Vector3 shoulderOrigin = (shoulderL.position + shoulderR.position) / 2;
 
-        bool hit1 = Physics.Raycast(thighOrigin, -transform.up, out RaycastHit thighHit, .5f, groundLayer);
-        bool hit2 = Physics.Raycast(shoulderOrigin, -transform.up, out RaycastHit shoulderHit, .6f, groundLayer);
+
+        bool hit1 = Physics.SphereCast(root.position, .05f, -transform.up, out RaycastHit backHit, .5f, groundLayer);
+        bool hit2 = Physics.SphereCast(trapezius.position, .05f, -transform.up, out RaycastHit frontHit, .5f, groundLayer);
         if (!hit1 || !hit2) return;
         
-        float l = Vector3.Distance(thighHit.point, shoulderHit.point);
-        float h = thighHit.point.y - shoulderHit.point.y;
+        float l = Vector3.Distance(backHit.point, frontHit.point);
+        float h = backHit.point.y - frontHit.point.y;
         float angle = Mathf.Asin(h / l) * -Mathf.Rad2Deg;
 
         targetRotation.SetPositionAndRotation(root.position, root.rotation);
         targetRotation.RotateAround(pivotPoint.position, transform.right, -angle);
 
+        root.GetPositionAndRotation(out Vector3 currentLocation, out Quaternion currentRotation);
         AdjustToInclineSmoothly();
+        AdjustNeck();
+
+        currentTranslationDiff = root.position - currentLocation;
+        currentRotationDiff = root.rotation * Quaternion.Inverse(currentRotation);
+
     }
 
     void AdjustToInclineSmoothly()
     {
-        positionProgress = Vector3.MoveTowards(positionProgress, targetRotation.position, maxDistanceDelta * Time.deltaTime);
-        rotationProgress = Quaternion.Euler(rotationProgress.eulerAngles.x, root.eulerAngles.y, root.eulerAngles.z);
-        rotationProgress = Quaternion.RotateTowards(rotationProgress, targetRotation.rotation, maxDegreesDelta * Time.deltaTime);
-        root.SetPositionAndRotation(positionProgress, rotationProgress);
+        rootRotationProgress = Quaternion.Euler(rootRotationProgress.eulerAngles.x, root.eulerAngles.y, root.eulerAngles.z);
+        rootRotationProgress = Quaternion.RotateTowards(rootRotationProgress, targetRotation.rotation, rootRotationSpeed * Time.deltaTime);
+        root.SetPositionAndRotation(targetRotation.position, rootRotationProgress);
+    }
+
+    void AdjustNeck()
+    {
+        if (Mathf.Abs(neck.localEulerAngles.x - localNeckRestingAngle) < 10) return; 
+        neckRotationProgress = Mathf.MoveTowardsAngle(neckRotationProgress, localNeckRestingAngle, neckRotationSpeed);
+        neckRotationProgress = Extensions.ClampAngle(neckRotationProgress, neck.eulerAngles.x - maxNeckDeltaAngle, neck.eulerAngles.x + maxNeckDeltaAngle);
+        neck.rotation = Quaternion.Euler(neckRotationProgress, neck.eulerAngles.y, neck.eulerAngles.z);
     }
 }
